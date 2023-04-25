69defd5c8e676d962b192d383b534f64da67d468
Hlor IV <Hlor IV@HLORKA-II>
2023-04-25 17:50:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Oper_perecluchenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод кириллического символа
+            Console.Write("Введите кириллический символ: ");
+            char symbol = Console.ReadKey().KeyChar;
+
+            // Получение кода символа по таблице Unicode
+            int code = (int)symbol;
+
+            // Вычисление следующего символа
+            char nextSymbol = (char)(code + 1);
+
+            // Вычисление остатка от деления на 4
+            int remainder = code % 4;
+
+            // Определение скобок для ответа
+            string brackets = "";
+            switch (remainder)
+            {
+                case 0:
+                    brackets = "()";
+                    break;
+                case 1:
+                    brackets = "[]";
+                    break;
+                case 2:
+                    brackets = "<>";
+                    break;
+                case 3:
+                    brackets = "{}";
+                    break;
+            }
+
+            // Вывод ответа 
+            Console.WriteLine($"\nСледующий символ: {brackets[0]}{nextSymbol}{brackets[1]}");
+        }
+    }
+}
